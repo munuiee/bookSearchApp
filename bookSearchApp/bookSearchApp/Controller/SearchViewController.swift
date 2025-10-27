f6715e39afba1ea775a021e4fb4fa01062ba459b
jyeee <munuiee@icloud.com>
2025-10-27 14:56:34 +0900

    Feat: 탭바 생성하기

---
+++ bookSearchApp/bookSearchApp/Controller/SearchViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController {
+    
+    private let searchBar = UITextField()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
